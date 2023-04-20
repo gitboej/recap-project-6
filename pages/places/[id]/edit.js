a40b54018ab7141cb308cb6a1161a45324eb9c70
Lars Bonus <lars.bonus@outlook.com>
2023-04-20 15:24:00 +0200

    add edit function

--- pages/places/[id]/edit.js
+++ pages/places/[id]/edit.js
@@ -1,8 +1,23 @@
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import useSWR from 'swr';
-import Form from '../../../components/Form.js';
-import { StyledLink } from '../../../components/StyledLink.js';
+import { useRouter } from "next/router";
+import Link from "next/link";
+import useSWR from "swr";
+import Form from "../../../components/Form.js";
+import { StyledLink } from "../../../components/StyledLink.js";
+import useSWRMutation from "swr/mutation";
+
+async function sendRequest(url, { arg }) {
+  const response = await fetch(url, {
+    method: "PATCH",
+    body: JSON.stringify(arg),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    console.error(`Error: ${response.status}`);
+  }
+}
 
 export default function EditPage() {
   const router = useRouter();
@@ -10,11 +25,20 @@ export default function EditPage() {
   const { id } = router.query;
   const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
 
+  const { trigger, isMutating } = useSWRMutation(
+    `/api/places/${id}`,
+    sendRequest
+  );
+
   async function editPlace(place) {
-    console.log('Place edited (but not really...');
+    place.preventDefault();
+
+    const formData = new FormData(place.target);
+    const placeData = Object.fromEntries(formData);
+    trigger(placeData);
   }
 
-  if (!isReady || isLoading || error) return <h2>Loading...</h2>;
+  if (!isReady || isLoading || isMutating || error) return <h2>Loading...</h2>;
 
   return (
     <>
@@ -22,7 +46,7 @@ export default function EditPage() {
       <Link href={`/places/${id}`} passHref legacyBehavior>
         <StyledLink justifySelf="start">back</StyledLink>
       </Link>
-      <Form onSubmit={editPlace} formName={'edit-place'} defaultData={place} />
+      <Form onSubmit={editPlace} formName={"edit-place"} defaultData={place} />
     </>
   );
 }
